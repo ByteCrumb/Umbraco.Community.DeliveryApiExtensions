84edb48cff7aa7fb8ddc26263ff441c18d2eb368
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-11-04 17:59:48 +0100

    Include angular controllers in main bundle (#5)

---
+++ src/UmbracoDeliveryApiExtensions.UI/src/controllers/preview.controller.js
@@ -0,0 +1,45 @@
+import angular from 'angular';
+
+angular
+  .module('umbraco')
+  .controller(
+    'Umbraco.Community.DeliveryApiExtensions.Preview',
+    [
+      '$scope', '$element', '$routeParams', 'contentAppHelper', 'eventsService',
+      function ($scope, $element, $routeParams, contentAppHelper, eventsService) {
+        const vm = this;
+
+        registerAsKnownContentApp();
+
+        vm.apiPath = $scope.model.viewModel.apiPath;
+        vm.entityType = $scope.model.viewModel.entityType;
+        vm.culture = $routeParams.cculture || $routeParams.mculture;
+
+        updateIsPublished();
+        const stateChangeWatch = $scope.$watch('variantContent.state', updateIsPublished);
+        const contentSavedWatch = eventsService.on('content.saved', updatePreview);
+
+        vm.$onDestroy = function () {
+          stateChangeWatch();
+          contentSavedWatch();
+        };
+
+        // Ensures the content save/publish are displayed
+        function registerAsKnownContentApp() {
+          const contentAppAlias = 'deliveryApiPreview';
+          if (contentAppHelper.CONTENT_BASED_APPS.indexOf(contentAppAlias) === -1) {
+            contentAppHelper.CONTENT_BASED_APPS.push(contentAppAlias);
+          }
+        }
+
+        function updateIsPublished() {
+          vm.isPublished = $scope.variantContent?.state !== 'Draft';
+        }
+
+        function updatePreview() {
+          updateIsPublished();
+          $element.find('bc-api-preview')[0].updateResponse();
+        }
+      }
+    ]
+  );
