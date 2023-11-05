a7a8b596a9e7f7408f13a9ef396b1c444843c0fc
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2023-11-05 18:34:49 +0100

    Added playwright folder as a project in VS

--- tests/UmbracoDeliveryApiExtensions.Playwright/playwright.config.ts
+++ tests/UmbracoDeliveryApiExtensions.Playwright/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: 'html',
   use: {
     baseURL: process.env.URL,
@@ -29,5 +29,6 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
     ignoreHTTPSErrors: true,
     stdout: process.env.CI ? "ignore": "pipe",
+    cwd: '..\\UmbracoDeliveryApiExtensions.TestSite'
   },
 });
