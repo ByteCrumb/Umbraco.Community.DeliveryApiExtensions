dcd999d476fae6164749f954450f40c602fa5594
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2023-11-06 23:41:21 +0100

    Adding sample content

--- tests/UmbracoDeliveryApiExtensions.Tests.Orval/src/api/umbraco-api.ts
+++ tests/UmbracoDeliveryApiExtensions.Tests.Orval/src/api/umbraco-api.ts
@@ -19,6 +19,8 @@ export type GetMediaItemByIdParams = {
 expand?: string;
 };
 
+export type GetMediaItemByPath404 = ProblemDetails | HttpValidationProblemDetails;
+
 export type GetMediaItemByPathParams = {
 /**
  * Defines the properties that should be expanded in the response. Refer to [the documentation](https://docs.umbraco.com/umbraco-cms/reference/content-delivery-api#query-parameters) for more details on this.
@@ -74,6 +76,8 @@ export type GetContentItemByIdParams = {
 expand?: string;
 };
 
+export type GetContentItemByPath404 = ProblemDetails | HttpValidationProblemDetails;
+
 export type GetContentItemByPath401 = ProblemDetails | HttpValidationProblemDetails;
 
 export type GetContentItemByPathParams = {
@@ -90,6 +94,10 @@ id?: string[];
  */
 expand?: string;
 };
+
+export type GetContent404 = ProblemDetails | HttpValidationProblemDetails;
+
+export type GetContent400 = ProblemDetails | HttpValidationProblemDetails;
 
 export type GetContentParams = {
 /**
@@ -118,6 +126,211 @@ take?: number;
 expand?: string;
 };
 
+export type TestPagePropertiesModelUserPicker = unknown | null;
+
+export type TestPagePropertiesModelMediaPickerItem = ApiMediaWithCropsModel | ApiMediaWithCropsResponseModel;
+
+export type TestPagePropertiesModel = TestCompositionPropertiesModel & TestComposition2PropertiesModel & {
+  blockGrid?: ApiBlockGridModel;
+  blockList?: ApiBlockListModel;
+  checkboxList?: string[] | null;
+  colorPicker?: PickedColorModel;
+  contentPicker?: IApiContentModel;
+  datePicker?: string | null;
+  datePickerWithTime?: string | null;
+  decimal?: number | null;
+  dropdown?: string | null;
+  email?: string | null;
+  eyeDropperColorPicker?: string | null;
+  imageCropper?: ApiImageCropperValueModel;
+  markdown?: string | null;
+  mediaPicker?: TestPagePropertiesModelMediaPickerItem[] | null;
+  memberGroupPicker?: string[] | null;
+  memberPicker?: string | null;
+  multinodeTreepicker?: IApiContentModel[] | null;
+  numeric?: number | null;
+  radiobox?: string | null;
+  repeatableTextstrings?: string[] | null;
+  richText?: RichTextModel;
+  slider?: number | null;
+  tags?: string[] | null;
+  textArea?: string | null;
+  textString?: string | null;
+  toggle?: boolean | null;
+  uploadFile?: string | null;
+  urlPicker?: ApiLinkModel[] | null;
+  userPicker?: TestPagePropertiesModelUserPicker;
+};
+
+export type TestPageInvariantPropertiesModelUserPicker = unknown | null;
+
+export type TestPageInvariantPropertiesModelMediaPickerItem = ApiMediaWithCropsModel | ApiMediaWithCropsResponseModel;
+
+export type TestPageInvariantPropertiesModel = TestCompositionPropertiesModel & TestComposition2PropertiesModel & {
+  blockGrid?: ApiBlockGridModel;
+  blockList?: ApiBlockListModel;
+  checkboxList?: string[] | null;
+  colorPicker?: PickedColorModel;
+  contentPicker?: IApiContentModel;
+  datePicker?: string | null;
+  datePickerWithTime?: string | null;
+  decimal?: number | null;
+  dropdown?: string | null;
+  email?: string | null;
+  eyeDropperColorPicker?: string | null;
+  imageCropper?: ApiImageCropperValueModel;
+  markdown?: string | null;
+  mediaPicker?: TestPageInvariantPropertiesModelMediaPickerItem[] | null;
+  memberGroupPicker?: string[] | null;
+  memberPicker?: string | null;
+  multinodeTreepicker?: IApiContentModel[] | null;
+  numeric?: number | null;
+  radiobox?: string | null;
+  repeatableTextstrings?: string[] | null;
+  richText?: RichTextModel;
+  slider?: number | null;
+  tags?: string[] | null;
+  textArea?: string | null;
+  textString?: string | null;
+  toggle?: boolean | null;
+  uploadFile?: string | null;
+  urlPicker?: ApiLinkModel[] | null;
+  userPicker?: TestPageInvariantPropertiesModelUserPicker;
+};
+
+export type TestPageInvariantElementModelContentType = typeof TestPageInvariantElementModelContentType[keyof typeof TestPageInvariantElementModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageInvariantElementModelContentType = {
+  testPageInvariant: 'testPageInvariant',
+} as const;
+
+export type TestPageInvariantElementModel = IApiElementModelBase & {
+  contentType: TestPageInvariantElementModelContentType;
+  properties?: TestPageInvariantPropertiesModel;
+};
+
+export type TestPageInvariantContentResponseModelContentType = typeof TestPageInvariantContentResponseModelContentType[keyof typeof TestPageInvariantContentResponseModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageInvariantContentResponseModelContentType = {
+  testPageInvariant: 'testPageInvariant',
+} as const;
+
+export type TestPageInvariantContentResponseModel = IApiContentResponseModelBase & TestPageInvariantContentModel & {
+  contentType: TestPageInvariantContentResponseModelContentType;
+};
+
+export type TestPageInvariantContentModelContentType = typeof TestPageInvariantContentModelContentType[keyof typeof TestPageInvariantContentModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageInvariantContentModelContentType = {
+  testPageInvariant: 'testPageInvariant',
+} as const;
+
+export type TestPageInvariantContentModel = IApiContentModelBase & TestPageInvariantElementModel & {
+  contentType: TestPageInvariantContentModelContentType;
+};
+
+export type TestPageElementModelContentType = typeof TestPageElementModelContentType[keyof typeof TestPageElementModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageElementModelContentType = {
+  testPage: 'testPage',
+} as const;
+
+export type TestPageElementModel = IApiElementModelBase & {
+  contentType: TestPageElementModelContentType;
+  properties?: TestPagePropertiesModel;
+};
+
+export type TestPageContentResponseModelContentType = typeof TestPageContentResponseModelContentType[keyof typeof TestPageContentResponseModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageContentResponseModelContentType = {
+  testPage: 'testPage',
+} as const;
+
+export type TestPageContentResponseModel = IApiContentResponseModelBase & TestPageContentModel & {
+  contentType: TestPageContentResponseModelContentType;
+};
+
+export type TestPageContentModelContentType = typeof TestPageContentModelContentType[keyof typeof TestPageContentModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestPageContentModelContentType = {
+  testPage: 'testPage',
+} as const;
+
+export type TestPageContentModel = IApiContentModelBase & TestPageElementModel & {
+  contentType: TestPageContentModelContentType;
+};
+
+export interface TestCompositionPropertiesModel {
+  sharedString?: string | null;
+  sharedToggle?: boolean | null;
+}
+
+export type TestCompositionElementModelContentType = typeof TestCompositionElementModelContentType[keyof typeof TestCompositionElementModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestCompositionElementModelContentType = {
+  testComposition: 'testComposition',
+} as const;
+
+export type TestCompositionElementModel = IApiElementModelBase & {
+  contentType: TestCompositionElementModelContentType;
+  properties?: TestCompositionPropertiesModel;
+};
+
+export interface TestComposition2PropertiesModel {
+  sharedRadiobox?: string | null;
+  sharedRichText?: RichTextModel;
+}
+
+export type TestComposition2ElementModelContentType = typeof TestComposition2ElementModelContentType[keyof typeof TestComposition2ElementModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestComposition2ElementModelContentType = {
+  testComposition2: 'testComposition2',
+} as const;
+
+export type TestComposition2ElementModel = IApiElementModelBase & {
+  contentType: TestComposition2ElementModelContentType;
+  properties?: TestComposition2PropertiesModel;
+};
+
+export type TestBlockPropertiesModel = TestCompositionPropertiesModel & {
+  blocks?: ApiBlockListModel;
+  multinodeTreepicker?: IApiContentModel[] | null;
+  string?: string | null;
+};
+
+export type TestBlockElementModelContentType = typeof TestBlockElementModelContentType[keyof typeof TestBlockElementModelContentType];
+
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const TestBlockElementModelContentType = {
+  testBlock: 'testBlock',
+} as const;
+
+export type TestBlockElementModel = IApiElementModelBase & {
+  contentType: TestBlockElementModelContentType;
+  properties?: TestBlockPropertiesModel;
+};
+
+export interface RichTextModel {
+  markup?: string;
+}
+
 export interface ProblemDetails {
   detail?: string | null;
   instance?: string | null;
@@ -127,13 +340,10 @@ export interface ProblemDetails {
   [key: string]: unknown;
 }
 
-export type GetMediaItemByPath404 = ProblemDetails | HttpValidationProblemDetails;
-
-export type GetContentItemByPath404 = ProblemDetails | HttpValidationProblemDetails;
-
-export type GetContent404 = ProblemDetails | HttpValidationProblemDetails;
-
-export type GetContent400 = ProblemDetails | HttpValidationProblemDetails;
+export interface PickedColorModel {
+  color?: string;
+  label?: string;
+}
 
 export interface PagedIApiContentResponseModel {
   items: IApiContentResponseModel[];
@@ -145,46 +355,15 @@ export interface PagedApiMediaWithCropsResponseModel {
   total: number;
 }
 
-export interface PageHomePropertiesModel {
-  text?: string | null;
-}
-
-export type PageHomeElementModelContentType = typeof PageHomeElementModelContentType[keyof typeof PageHomeElementModelContentType];
+export type LinkTypeModel = typeof LinkTypeModel[keyof typeof LinkTypeModel];
 
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
-export const PageHomeElementModelContentType = {
-  pageHome: 'pageHome',
+export const LinkTypeModel = {
+  Content: 'Content',
+  Media: 'Media',
+  External: 'External',
 } as const;
-
-export type PageHomeElementModel = IApiElementModelBase & {
-  contentType: PageHomeElementModelContentType;
-  properties?: PageHomePropertiesModel;
-};
-
-export type PageHomeContentResponseModelContentType = typeof PageHomeContentResponseModelContentType[keyof typeof PageHomeContentResponseModelContentType];
-
-
-// eslint-disable-next-line @typescript-eslint/no-redeclare
-export const PageHomeContentResponseModelContentType = {
-  pageHome: 'pageHome',
-} as const;
-
-export type PageHomeContentModelContentType = typeof PageHomeContentModelContentType[keyof typeof PageHomeContentModelContentType];
-
-
-// eslint-disable-next-line @typescript-eslint/no-redeclare
-export const PageHomeContentModelContentType = {
-  pageHome: 'pageHome',
-} as const;
-
-export type PageHomeContentModel = IApiContentModelBase & PageHomeElementModel & {
-  contentType: PageHomeContentModelContentType;
-};
-
-export type PageHomeContentResponseModel = IApiContentResponseModelBase & PageHomeContentModel & {
-  contentType: PageHomeContentResponseModelContentType;
-};
 
 export interface ImageCropperFocalPointModel {
   left?: number;
@@ -213,7 +392,7 @@ export interface IApiElementModelBase {
   readonly properties?: IApiElementModelBaseProperties;
 }
 
-export type IApiElementModel = PageHomeElementModel;
+export type IApiElementModel = TestPageElementModel | TestCompositionElementModel | TestBlockElementModel | TestComposition2ElementModel | TestPageInvariantElementModel;
 
 export interface IApiContentStartItemModel {
   readonly id?: string;
@@ -229,18 +408,7 @@ export type IApiContentResponseModelBaseProperties = {[key: string]: unknown};
 
 export type IApiContentResponseModelBaseCultures = {[key: string]: IApiContentRouteModel};
 
-export type IApiContentResponseModelBase = IApiContentModelBase & {
-  readonly contentType?: string;
-  readonly createDate?: string;
-  readonly cultures?: IApiContentResponseModelBaseCultures;
-  readonly id?: string;
-  readonly name?: string | null;
-  readonly properties?: IApiContentResponseModelBaseProperties;
-  route?: IApiContentRouteModel;
-  readonly updateDate?: string;
-};
-
-export type IApiContentResponseModel = PageHomeContentResponseModel;
+export type IApiContentResponseModel = TestPageContentResponseModel | TestPageInvariantContentResponseModel;
 
 export type IApiContentModelBaseProperties = {[key: string]: unknown};
 
@@ -254,7 +422,18 @@ export type IApiContentModelBase = IApiElementModelBase & {
   readonly updateDate?: string;
 };
 
-export type IApiContentModel = PageHomeContentModel;
+export type IApiContentResponseModelBase = IApiContentModelBase & {
+  readonly contentType?: string;
+  readonly createDate?: string;
+  readonly cultures?: IApiContentResponseModelBaseCultures;
+  readonly id?: string;
+  readonly name?: string | null;
+  readonly properties?: IApiContentResponseModelBaseProperties;
+  route?: IApiContentRouteModel;
+  readonly updateDate?: string;
+};
+
+export type IApiContentModel = TestPageContentModel | TestPageInvariantContentModel;
 
 export type HttpValidationProblemDetailsErrors = {[key: string]: string[]};
 
@@ -284,6 +463,52 @@ export type ApiMediaWithCropsResponseModel = ApiMediaWithCropsModel & {
   path?: string;
   updateDate?: string;
 };
+
+export interface ApiLinkModel {
+  readonly destinationId?: string | null;
+  readonly destinationType?: string | null;
+  linkType?: LinkTypeModel;
+  route?: IApiContentRouteModel;
+  readonly target?: string | null;
+  readonly title?: string | null;
+  readonly url?: string | null;
+}
+
+export interface ApiImageCropperValueModel {
+  crops?: ImageCropperCropModel[] | null;
+  focalPoint?: ImageCropperFocalPointModel;
+  url?: string;
+}
+
+export interface ApiBlockListModel {
+  items?: ApiBlockListModelItemsItem[];
+}
+
+export interface ApiBlockItemModel {
+  content?: IApiElementModel;
+  settings?: IApiElementModel;
+}
+
+export interface ApiBlockGridAreaModel {
+  alias?: string;
+  columnSpan?: number;
+  items?: ApiBlockGridItemModel[];
+  rowSpan?: number;
+}
+
+export type ApiBlockGridItemModel = ApiBlockItemModel & {
+  areaGridColumns?: number;
+  areas?: ApiBlockGridAreaModel[];
+  columnSpan?: number;
+  rowSpan?: number;
+};
+
+export type ApiBlockListModelItemsItem = ApiBlockItemModel | ApiBlockGridItemModel;
+
+export interface ApiBlockGridModel {
+  gridColumns?: number;
+  items?: ApiBlockGridItemModel[];
+}
 
 
 
