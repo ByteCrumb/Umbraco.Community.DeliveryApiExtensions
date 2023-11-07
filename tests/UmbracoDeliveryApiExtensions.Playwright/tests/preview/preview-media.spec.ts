7f34c1b76b67c27f6828bf3b0fb3d6624b6bb06d
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2023-11-08 00:17:50 +0100

    Initial playwright setup (#4)
    * Initial playwright setup
    
    * Configure playwright web server to run the test site
    
    * Added playwright test to check that the content app is not visible on a new document
    
    * Added playwright folder as a project in VS
    
    * Moved solution to the root + re-ordered projects for default startup project
    
    * Added first media test.
    
    * Additional configuration

---
+++ tests/UmbracoDeliveryApiExtensions.Playwright/tests/preview/preview-media.spec.ts
@@ -0,0 +1,43 @@
+﻿import {expect} from '@playwright/test';
+import {type ApiHelpers, ConstantHelper, test} from '@umbraco/playwright-testhelpers';
+
+test.describe('API preview - Media', () => {
+  const mediaName = 'PlaywrightTestMedia';
+
+  test.beforeEach(async ({umbracoApi}) => {
+    await umbracoApi.login();
+
+    await cleanTestMedia(umbracoApi);
+    await createTestMedia(umbracoApi);
+  });
+
+  test.afterEach(async ({umbracoApi}) => {
+    await cleanTestMedia(umbracoApi);
+  });
+
+  test('Preview content app is visible in saved media', async ({page, umbracoUi}) => {
+    // Go to test media (PR to add helper method: https://github.com/umbraco/Umbraco.Playwright.Testhelpers/pull/34)
+    await umbracoUi.goToSection(ConstantHelper.sections.media);
+    await umbracoUi.refreshMediaTree();
+    await umbracoUi.clickDataElementByElementName('tree-item-' + mediaName);
+
+    // Check that the content app is visible
+    const contentAppLocator = page.locator('button[data-element="sub-view-deliveryApiPreview"]');
+    await expect(contentAppLocator).toBeVisible();
+
+    // Click on the content app
+    await contentAppLocator.click();
+
+    // Verify that the preview component is visible
+    const apiPreviewElement = page.locator('bc-api-preview');
+    await expect(apiPreviewElement).toBeVisible();
+  });
+
+  async function createTestMedia(umbracoApi: ApiHelpers) {
+    await umbracoApi.media.createDefaultFile(mediaName);
+  }
+
+  async function cleanTestMedia(umbracoApi: ApiHelpers) {
+    await umbracoApi.media.ensureNameNotExists(mediaName);
+  }
+});
