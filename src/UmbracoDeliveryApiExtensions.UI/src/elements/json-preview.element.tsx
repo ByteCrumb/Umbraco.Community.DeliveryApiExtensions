0f277df473330301ee354a7e702933a9275b3f39
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-11-08 18:05:18 +0100

    Create helper for react web components
    Ensure JSON Viewer value property is not mapped as an attribute

--- src/UmbracoDeliveryApiExtensions.UI/src/elements/json-preview.element.tsx
+++ src/UmbracoDeliveryApiExtensions.UI/src/elements/json-preview.element.tsx
@@ -1,7 +1,7 @@
-import r2wc from '@r2wc/react-to-web-component';
 import JsonView from '@uiw/react-json-view';
 import {type JsonViewProps} from '@uiw/react-json-view';
-import {defineElement} from '@umbraco-ui/uui';
+
+import defineReactElement from '../helpers/define-react-element';
 
 const WebReactJsonComponent = (props: Pick<JsonViewProps<Record<string, unknown>>, 'value'>) =>
   <JsonView displayDataTypes={false} shortenTextAfterLength={50} collapsed={2} value={props.value}>
@@ -9,10 +9,4 @@ const WebReactJsonComponent = (props: Pick<JsonViewProps<Record<string, unknown>
     <JsonView.CountInfo render={(_props, result) => Array.isArray(result.value) ? undefined : <span/> }/>
   </JsonView>;
 
-const WebReactJsonElement = r2wc(WebReactJsonComponent, {
-  props: {
-    value: 'json',
-  },
-});
-
-defineElement('bc-json-preview')(WebReactJsonElement);
+defineReactElement('bc-json-preview', WebReactJsonComponent, {props: {value: undefined}});
