c626dba78f94ec2ec8a40e13bdceb31f90809792
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-11-10 19:03:05 +0100

    Json viewer fixes and improvements
    Patched r2wc library types and not re-rendering on property setter
    Hide ellipsis if object or array has no entries
    Changed copy icons to umbraco ones

--- src/UmbracoDeliveryApiExtensions.UI/src/elements/json-preview.element.tsx
+++ src/UmbracoDeliveryApiExtensions.UI/src/elements/json-preview.element.tsx
@@ -1,12 +1,18 @@
 import JsonView from '@uiw/react-json-view';
 import {type JsonViewProps} from '@uiw/react-json-view';
+import {type UUIIconElement} from '@umbraco-ui/uui';
 
 import defineReactElement from '../helpers/define-react-element';
 
 const WebReactJsonComponent = (props: Pick<JsonViewProps<Record<string, unknown>>, 'value'>) =>
   <JsonView displayDataTypes={false} shortenTextAfterLength={50} collapsed={2} value={props.value}>
-    <JsonView.Null render={(props, result) => result.type === 'value' ? <span {...props}>null</span> : <span/>}/>
-    <JsonView.CountInfo render={(_props, result) => Array.isArray(result.value) ? undefined : <span/> }/>
+    <JsonView.Null render={(props, {type}) => type === 'value' ? <span {...props}>null</span> : <span/>}/>
+    <JsonView.CountInfo render={(_props, {value}) => Array.isArray(value) ? undefined : <span/> }/>
+    <JsonView.Ellipsis render={(_props, {value}) => Object.keys(value ?? {}).length === 0 ? <span>&nbsp;</span> : undefined }/>
+    <JsonView.Copied render={(props, _result) => {
+      const copied = 'data-copied' in props && Boolean(props['data-copied']);
+      return <uui-icon name='copy' style={{...props.style, color: copied ? 'var(--uui-color-positive)' : 'var(--uui-color-disabled-contrast)'}} onClick={copied ? undefined : props.onClick as unknown as React.MouseEventHandler<UUIIconElement>}></uui-icon>;
+    }}/>
   </JsonView>;
 
 defineReactElement('bc-json-preview', WebReactJsonComponent, {props: {value: undefined}});
--- src/UmbracoDeliveryApiExtensions.UI/src/helpers/define-react-element.ts
+++ src/UmbracoDeliveryApiExtensions.UI/src/helpers/define-react-element.ts
@@ -2,14 +2,22 @@ import type {R2WCOptions} from '@r2wc/core';
 import r2wc from '@r2wc/react-to-web-component';
 import {defineElement} from '@umbraco-ui/uui';
 
-export interface Options<Props> extends Omit<R2WCOptions<Props>, 'props'> {
-  props?: Partial<R2WCOptions<Props>['props']>;
+declare global {
+  namespace JSX {
+    // IntrinsicElementMap grabs all the standard HTML tags in the TS DOM lib.
+    interface IntrinsicElements extends IntrinsicElementMap { }
+
+    // The following are custom types, not part of TS's known JSX namespace:
+    type IntrinsicElementMap = {
+      [K in keyof HTMLElementTagNameMap]: HTMLElementTagNameMap[K] | React.DetailedHTMLProps<React.HTMLAttributes<HTMLElementTagNameMap[K]>, HTMLElementTagNameMap[K]>
+    };
+  }
 }
 
-export default function defineReactElement<Props extends Record<string, unknown>>(name: string, ReactComponent: React.ComponentType<Props>, options?: Options<Props>): void {
+export default function defineReactElement<Props extends Record<string, unknown>>(name: string, ReactComponent: React.ComponentType<Props>, options?: R2WCOptions<Props>): void {
   options ??= {};
   options.shadow ??= 'open';
 
-  const ReactWebComponentElement = r2wc(ReactComponent, options as R2WCOptions<Props>);
+  const ReactWebComponentElement = r2wc(ReactComponent, options);
   defineElement(name)(ReactWebComponentElement);
 }
