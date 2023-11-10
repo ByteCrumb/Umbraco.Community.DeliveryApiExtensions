831c73ecfcc4656ba44f1757b49c677a583c3391
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-11-10 19:07:49 +0100

    Added support for query dependent data files

--- src/UmbracoDeliveryApiExtensions.UI/vite.config.ts
+++ src/UmbracoDeliveryApiExtensions.UI/vite.config.ts
@@ -39,5 +39,16 @@ export default defineConfig(({mode}) => ({
   server: {
     port: 5173,
     strictPort: true,
+    proxy: {
+      // Add support for query param replacement in data files
+      // Syntax: {queryParamName|fallbackValue}
+      '^/src/data/.*%7B.+%7D': {
+        target: 'http://localhost:5173',
+        rewrite(path) {
+          const url = new URL(path, 'http://localhost');
+          return path.replace(/%7B(.+?)(?:%7C(.+?))?%7D/gi, (_, paramName: string, defaultValue: string) => url.searchParams.get(paramName) ?? defaultValue ?? '');
+        },
+      },
+    },
   },
 }));
