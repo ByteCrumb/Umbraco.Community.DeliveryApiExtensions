b656a35d6a6e31fdafd9726da68533237e5a6793
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-10-31 23:35:30 +0100

    Fix umbraco package version not matching nuget version

--- src/UmbracoDeliveryApiExtensions.UI/vite.config.ts
+++ src/UmbracoDeliveryApiExtensions.UI/vite.config.ts
@@ -1,7 +1,9 @@
-import {defineConfig} from 'vite';
+import {defineConfig, PluginOption} from 'vite';
+import {readFile, writeFile} from 'node:fs/promises';
 
 export default defineConfig(({mode}) => ({
   base: '/App_Plugins/DeliveryApiExtensions',
+  plugins: [replaceUmbracoPackageVersion],
   build: {
     lib: {
       entry: 'src/main.ts',
@@ -47,3 +49,24 @@ export default defineConfig(({mode}) => ({
     },
   },
 }));
+
+const replaceUmbracoPackageVersion: PluginOption = {
+  name: 'sync-umbraco-package-version',
+  async buildStart() {
+    const umbracoPackageJsonPath = 'public/umbraco-package.json';
+    const content = await readFile(umbracoPackageJsonPath, 'utf-8');
+    const currentVersion = content.match(/"version":\s*"([^"]+)"/)?.[1];
+    if(!currentVersion){
+      throw new Error('Missing version in umbraco-package.json');
+    }
+
+    const packageVersion = process.env.npm_package_version ?? '0.0.0';
+    if (currentVersion === packageVersion) {
+      return;
+    }
+
+    const updatedContent = content.replace(currentVersion, packageVersion);
+    await writeFile(umbracoPackageJsonPath, updatedContent);
+    console.log(`✓ Updated umbraco-package.json version to ${packageVersion}`);
+  },
+};
